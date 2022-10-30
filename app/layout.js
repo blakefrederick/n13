45c54e99c69dbf60f46f509e38ac47d35432b13b
Blake Frederick <bf@six12creative.com>
2022-10-30 13:52:33 -0700

    App has navigation

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -5,9 +7,15 @@ export default function RootLayout({ children }) {
         <title>N13</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </head>
-      <div>Layout start</div>
-      <body>{children}</body>
-      <div>Layout end</div>
+      <body>
+        <nav>
+          <Link href="/">Home</Link>|
+          <Link href="/1999">1999</Link>
+        </nav>      
+        <div>Layout header</div>
+        {children}
+        <div>Layout footer</div>
+      </body>
     </html>
   );
 }
