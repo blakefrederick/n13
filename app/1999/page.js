3c27108e095fddaab41d3f5f632f5262c101896d
Blake Frederick <bf@six12creative.com>
2022-11-01 23:33:31 -0700

    sleep for 2 seconds after fetching, before returning

--- app/1999/page.js
+++ app/1999/page.js
@@ -1,7 +1,9 @@
+import { setHttpClientAndAgentOptions } from 'next/dist/server/config'
 import { use } from 'react'
 
 async function get1999() {
     let response = await fetch("http://localhost:3000/data1999.json")
+    await new Promise(r => setTimeout(r, 2000))
     return response.json()
 }
 export default function Page() {
