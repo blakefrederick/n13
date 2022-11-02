df6b351c6602f04abf4e4e8268c4adfe111f142e
Blake Frederick <bf@six12creative.com>
2022-11-01 23:11:17 -0700

    Try new use hook

--- app/1999/page.js
+++ app/1999/page.js
@@ -1,4 +1,17 @@
-// This file maps to the index route (/1999)
+import { use } from 'react'
+
+async function get1999() {
+    let response = await fetch("http://localhost:3000/data1999.json")
+    return response.json()
+}
 export default function Page() {
-    return <h1>Hello, it's 1999!</h1>;
+    let data1999 = use(get1999())
+
+    return (
+        <ul>
+            {data1999.map((data) => (
+                <li key={data.id}>{data.title}</li>
+            ))}
+        </ul>
+    )
 }
